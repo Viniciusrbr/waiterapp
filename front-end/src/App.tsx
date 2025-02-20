c0985cc2cad4752224408124a264869f841555a5
viniciusrbr <viniciusgoes159@gmail.com>
2025-02-20 17:15:49 -0300

    web: feat: header component

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,11 @@
+import { Header } from './components/Header'
 import { GlobalStyles } from './styles/GlobalStyles'
 
 export default function App() {
   return (
     <>
       <GlobalStyles />
+      <Header />
       <h1>Hello World!</h1>
     </>
   )
