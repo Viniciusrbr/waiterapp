41e08025a0141a5d51b728b1d454a4774fe43a73
viniciusrbr <viniciusgoes159@gmail.com>
2025-02-24 19:04:58 -0300

    feat: menu component

--- mobile/src/components/Menu/index.tsx
+++ mobile/src/components/Menu/index.tsx
@@ -1,9 +1,45 @@
-import { Text } from "../Text";
-import { Container } from "./styles";
+import { FlatList } from 'react-native';
+import { Text } from '../Text';
+import { products } from '../../mocks/products';
+import {
+  ProductContainer,
+  ProductImage,
+  ProductDetails,
+  Separator,
+  AddToCardButton,
+} from './styles';
+import { formatCurrency } from '../../utils/formatCurrency';
+import { PlusCircle } from '../Icons/PlusCircle';
 
 export function Menu() {
   return (
-    <Container>
-    </Container>
+    <>
+      <FlatList
+        data={products}
+        style={{ marginTop: 32 }}
+        contentContainerStyle={{ paddingHorizontal: 24 }}
+        keyExtractor={product => product._id}
+        ItemSeparatorComponent={Separator}
+        renderItem={({ item: product }) => (
+          <ProductContainer>
+            <ProductImage
+              source={{ uri: `http://192.168.18.30:3001/uploads/${product.imagePath}` }}
+            />
+
+            <ProductDetails>
+              <Text weight='600'>{product.name}</Text>
+              <Text size={14} color='#666' style={{ marginVertical: 8 }}>
+                {product.description}
+              </Text>
+              <Text size={14} weight='600'>{formatCurrency(product.price)}</Text>
+            </ProductDetails>
+
+            <AddToCardButton>
+              <PlusCircle />
+            </AddToCardButton>
+          </ProductContainer>
+        )}
+      />
+    </>
   )
 }
--- mobile/src/mocks/products.ts
+++ mobile/src/mocks/products.ts
@@ -1,38 +1,38 @@
-const products = [
+export const products = [
   {
     _id: '6372e040f52e37ef85fe2c5e',
     name: 'Pizza quatro queijos',
     description: 'Deliciosa pizza quatro queijos com borda simples',
-    imagePath: '1668472896991-quatro-queijos.png',
+    imagePath: '1740004169737-quatro-queijos.png',
     price: 40,
     ingredients: [
       {
         name: 'Mussarela',
         icon: '🧀',
-        _id: '6372e040f52e37ef85fe2c5f'
+        _id: '67b65b49d547fc5b0118db20'
       },
       {
         name: 'Parmesão',
         icon: '🧀',
-        _id: '6372e040f52e37ef85fe2c60'
+        _id: '67b65b49d547fc5b0118db21'
       },
       {
         name: 'Gouda',
         icon: '🧀',
-        _id: '6372e040f52e37ef85fe2c61'
+        _id: '67b65b49d547fc5b0118db22'
       },
       {
         name: 'Brie',
         icon: '🧀',
-        _id: '6372e040f52e37ef85fe2c62'
+        _id: '67b65b49d547fc5b0118db23'
       }
     ],
   },
   {
-    _id: '6372e276a381106c0f854cb3',
+    _id: '67b66b31dac7058fc4c4f804',
     name: 'Coca cola',
     description: 'Coca cola lata geladinha topzera',
-    imagePath: '1668473462705-coca-cola.png',
+    imagePath: '1740008241918-coca-cola.png',
     price: 7,
     ingredients: [],
   },
