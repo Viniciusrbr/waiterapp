27de23cfea49c43de60f701079d820617b65487d
viniciusrbr <viniciusgoes159@gmail.com>
2025-02-24 20:03:44 -0300

    feat: table modal

--- mobile/src/Main/index.tsx
+++ mobile/src/Main/index.tsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import { Button } from "../components/Button";
 import { Categories } from "../components/Categories";
 import { Header } from "../components/Header";
 import { Menu } from "../components/Menu";
+import { TableModal } from "../components/TableModal";
 import { Container, CategoriesContainer, MenuContainer, Footer, FooterContainer } from "./styles";
 
 export function Main() {
+  const [isTableModalVisible, setIsTableModalVisible] = useState(false);
+  const [selectedTable, setSelectedTable] = useState('');
+
+  function handleSaveTable(table: string) {
+    setSelectedTable(table);
+  }
+
   return (
     <>
       <Container>
@@ -22,11 +31,19 @@ export function Main() {
 
       <Footer>
         <FooterContainer>
-          <Button onPress={() => { }}>
-            Novo Pedido
-          </Button>
+          {!selectedTable && (
+            <Button onPress={() => setIsTableModalVisible(true)}>
+              Novo Pedido
+            </Button>
+          )}
         </FooterContainer>
       </Footer>
+
+      <TableModal
+        visible={isTableModalVisible}
+        onClose={() => setIsTableModalVisible(false)}
+        onSave={handleSaveTable}
+      />
     </>
   );
 }
---
+++ mobile/src/components/TableModal/index.tsx
@@ -0,0 +1,64 @@
+import { Overlay, ModalBody, Header, Form, Input } from './styles';
+import { Modal, TouchableOpacity, Platform } from 'react-native';
+
+import { Close } from '../Icons/Close';
+
+import { Text } from '../Text';
+import { Button } from '../Button';
+
+import { useState } from 'react';
+
+interface TableModalProps {
+  visible: boolean;
+  onClose: () => void;
+  onSave: (table: string) => void;
+}
+
+export function TableModal({ visible, onClose, onSave }: TableModalProps) {
+
+  const [table, setTable] = useState('');
+
+  function handleSave() {
+    setTable('');
+    onSave(table);
+    onClose();
+  }
+
+  return (
+    <Modal
+      visible={visible}
+      transparent
+      animationType='fade'
+    >
+
+      <Overlay behavior={Platform.OS === 'android' ? 'height' : 'padding'}>
+        <ModalBody>
+          <Header>
+            <Text weight='600'>Informe a mesa</Text>
+
+            <TouchableOpacity onPress={onClose}>
+              <Close color='#666' />
+            </TouchableOpacity>
+          </Header>
+
+          <Form>
+            <Input
+              value={table}
+              placeholder='Número da mesa'
+              placeholderTextColor='#666'
+              keyboardType='number-pad'
+              onChangeText={value => setTable(value)}
+            />
+
+            <Button
+              onPress={handleSave}
+              disabled={table.length === 0}
+            >
+              Salvar
+            </Button>
+          </Form>
+        </ModalBody>
+      </Overlay>
+    </Modal>
+  );
+}
